2cce7c40ae34f28851d23fa7585e18fe3351be9e
Mohana LK Lingabathina <mohanlingabathina8@gmail.com>
2026-09-18 18:14:58 +0530

    Restore exact medical image operations from original

--- lumina/image_processing/medical.py
+++ lumina/image_processing/medical.py
@@ -1,7 +1,7 @@
-import numpy as np
-from PIL import Image, ImageOps
+import os
 import cv2
-
+import numpy as np
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps, ImageDraw, ImageFont
 from ..utils.image import pil_to_cv2, cv2_to_pil
 
 def clahe(img, params):
