b931f614f3df25632cfcb03889b4ede001b1d2b2
Mohana LK Lingabathina <mohanlingabathina8@gmail.com>
2026-09-18 18:06:40 +0530

    Extract image utility functions

---
+++ lumina/utils/image.py
@@ -0,0 +1,22 @@
+import base64
+import io
+
+import cv2
+import numpy as np
+from PIL import Image
+from flask import session
+
+def pil_to_base64(img):
+
+def file_to_pil(file):
+
+def pil_to_cv2(img):
+
+def cv2_to_pil(arr):
+
+def pil_to_bytes(img, quality=85):
+
+def is_rate_limit(err_str):
+
+def check_guest_limit():
+
