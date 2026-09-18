97d7d4d2bb0ab0974eafad9b38dfb5a441c735c0
Mohana LK Lingabathina <mohanlingabathina8@gmail.com>
2026-09-18 18:15:01 +0530

    Restore exact detection image operations from original

--- lumina/image_processing/detection.py
+++ lumina/image_processing/detection.py
@@ -1,7 +1,7 @@
-import numpy as np
-from PIL import Image, ImageFilter, ImageOps, ImageEnhance
+import os
 import cv2
-
+import numpy as np
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps, ImageDraw, ImageFont
 from ..utils.image import pil_to_cv2, cv2_to_pil
 
 def face_detect(img, params):
